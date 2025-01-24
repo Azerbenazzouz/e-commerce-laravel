6822205c9d34416c86c63a1afd0a99896a3fc994
Azer Ben Azzouz <benazzouzazer@gmail.com>
2025-01-24 17:23:57 +0100

    Ajout de la validation des requêtes d'authentification avec AuthRequest et mise à jour de la méthode authenticate dans AuthController

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AuthRequest;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller {
 
-    public function authenticate() {
+    public function authenticate(AuthRequest $request) {
         return response()->json(['message' => 'Hello World!']);
     }
 }
--- app/Http/Requests/AuthRequest.php
+++ app/Http/Requests/AuthRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class AuthRequest extends FormRequest
 {
@@ -11,7 +13,7 @@ class AuthRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +24,19 @@ class AuthRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'email' => 'required|email',
+            'password' => 'required|string',
         ];
     }
+
+    public function failedValidation(Validator $validator){
+        throw new HttpResponseException(
+            response()->json([
+                'errors' => $validator->errors(),
+                'status' => 422
+            ], 422)
+        );
+    }
+
 }
+
