e1c614ecdb9c884b9a49a9e27bd42dbf4c2ba4e6
Azer Ben Azzouz <benazzouzazer@gmail.com>
2025-01-24 19:32:28 +0100

    Ajout de méthodes statiques pour la gestion des réponses API dans ApiResource

--- app/Http/Resources/ApiResource.php
+++ app/Http/Resources/ApiResource.php
@@ -7,6 +7,36 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class ApiResource extends JsonResource
 {
+
+    public static function ok(array $data = [], string $message = '', int $code = 200) {
+        return [
+            'status' => true,
+            'code' => $code,
+            'message' => $message,
+            'data' => $data,
+            'timestamp' => now()
+        ];
+    }
+
+    public static function error(array $errors = [], string $message = '', int $code = 400) {
+        return [
+            'status' => false,
+            'code' => $code,
+            'message' => $message,
+            'errors' => $errors,
+            'timestamp' => now()
+        ];
+    }
+
+    public static function message(string $message, int $code = 200) {
+        return [
+            'status' => $code === 200,
+            'message' => $message,
+            'code' => $code,
+            'timestamp' => now()
+        ];
+    }
+
     /**
      * Transform the resource into an array.
      *
