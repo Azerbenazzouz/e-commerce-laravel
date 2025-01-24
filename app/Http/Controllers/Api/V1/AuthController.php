3313532e2a1823b63587f94e07fe243187060478
Azer Ben Azzouz <benazzouzazer@gmail.com>
2025-01-24 16:55:15 +0100

    Ajout de l'authentification API avec une nouvelle méthode dans AuthController, mise à jour du DatabaseSeeder et création d'une requête de validation pour l'authentification

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class AuthController extends Controller
-{
-    //
+class AuthController extends Controller {
+
+    public function authenticate() {
+        return response()->json(['message' => 'Hello World!']);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\V1\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/v1/auth/login', [AuthController::class, 'authenticate']);
