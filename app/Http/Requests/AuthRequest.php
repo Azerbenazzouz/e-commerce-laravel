c253cac34fe3e87db45ce7bb1cc2d12c9461ff32
Azer Ben Azzouz <benazzouzazer@gmail.com>
2025-01-24 17:29:35 +0100

    Refactor AuthRequest en héritant de BaseRequest et suppression de la logique de validation redondante

--- app/Http/Requests/AuthRequest.php
+++ app/Http/Requests/AuthRequest.php
@@ -1,41 +1,17 @@
 <?php
-
 namespace App\Http\Requests;
 
-use Illuminate\Contracts\Validation\Validator;
-use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Http\Exceptions\HttpResponseException;
+class AuthRequest extends BaseRequest {
 
-class AuthRequest extends FormRequest
-{
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
+    public function authorize(): bool {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
-    {
+    public function rules(): array {
         return [
             'email' => 'required|email',
             'password' => 'required|string',
         ];
-    }
-
-    public function failedValidation(Validator $validator){
-        throw new HttpResponseException(
-            response()->json([
-                'errors' => $validator->errors(),
-                'status' => 422
-            ], 422)
-        );
     }
 
 }
---
+++ app/Http/Requests/BaseRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class BaseRequest extends FormRequest {
+
+    public function failedValidation(Validator $validator){
+        throw new HttpResponseException(
+            response()->json([
+                'errors' => $validator->errors(),
+                'status' => 422
+            ], 422)
+        );
+    }
+
+}
